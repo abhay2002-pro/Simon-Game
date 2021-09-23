4d23916565fb6953be90b06206996c22f602ab26
Abhay Kumar Ray <abhayray2002@gmail.com>
2021-09-24 01:00:32 +0530

    game.js added

---
+++ game.js
@@ -0,0 +1,8 @@
+var buttonColours = ["red","blue","green","yellow"];
+
+function nextsequence(){
+    var randomNumber = Math.floor(Math.random() * 4);
+    return randomNumber;
+}
+
+// console.log(nextsequence());
